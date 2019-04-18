e979541a717981545517fda8d1c528d2d8db97b5
Afreet <tempacc4897@gmail.com>
2019-04-18 12:21:28 +0530

    Updated the share Activity with weebview based UI for sharing post

--- app/src/main/java/com/afreet/campusconnect/Share/ShareActivity.java
+++ app/src/main/java/com/afreet/campusconnect/Share/ShareActivity.java
@@ -1,153 +1,255 @@
 package com.afreet.campusconnect.Share;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.graphics.PorterDuff;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Patterns;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.Spinner;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.afreet.campusconnect.Home.HomeActivity;
 import com.afreet.campusconnect.Notice.NoticeActivity;
 import com.afreet.campusconnect.Offer.OfferActivity;
 import com.afreet.campusconnect.Profile.ProfileActivity;
 import com.afreet.campusconnect.R;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ShareActivity extends AppCompatActivity {
 
     private static final int ACTIVITY_NUM = 3;
-    private static final int PICK_IMAGE = 2;
     private TabLayout tabLayout;
     private Context mContext = ShareActivity.this;
-
-    private Spinner spinnerUserCategory;
-    private EditText inputPostCaption, inputPostLink;
-    private RelativeLayout btnSelectImage;
-    private ImageView ivSelectedImage;
-    private FloatingActionButton fabPublishPost;
-
-    private int imageSelect = 0;
-    private Uri imageUri;
-
+    private static final int FCR = 1;
+    private WebView shareWebView;
+    private String mCM;
+    private ValueCallback<Uri> mUM;
+    private ValueCallback<Uri[]> mUMA;
+    //select whether you want to upload multiple files (set 'true' for yes)
+    private boolean multiple_files = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(Build.VERSION.SDK_INT >= 21){
+            Uri[] results = null;
+            //checking if response is positive
+            if(resultCode== Activity.RESULT_OK){
+                if(requestCode == FCR){
+                    if(null == mUMA){
+                        return;
+                    }
+                    if(intent == null || intent.getData() == null){
+                        if(mCM != null){
+                            results = new Uri[]{Uri.parse(mCM)};
+                        }
+                    }else{
+                        String dataString = intent.getDataString();
+                        if(dataString != null){
+                            results = new Uri[]{Uri.parse(dataString)};
+                        } else {
+                            if(multiple_files) {
+                                if (intent.getClipData() != null) {
+                                    final int numSelectedFiles = intent.getClipData().getItemCount();
+                                    results = new Uri[numSelectedFiles];
+                                    for (int i = 0; i < numSelectedFiles; i++) {
+                                        results[i] = intent.getClipData().getItemAt(i).getUri();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            mUMA.onReceiveValue(results);
+            mUMA = null;
+        }else{
+            if(requestCode == FCR){
+                if(null == mUM) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                mUM.onReceiveValue(result);
+                mUM = null;
+            }
+        }
+    }
+
+    @SuppressLint({"SetJavaScriptEnabled", "WrongViewCast"})
+    @SuppressWarnings({"findViewById", "RedundantCast"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
 
-        spinnerUserCategory = (Spinner) findViewById(R.id.spinner_userCategory);
-        inputPostCaption = (EditText) findViewById(R.id.input_postCaption);
-        inputPostLink = (EditText) findViewById(R.id.input_postLink);
-        btnSelectImage = (RelativeLayout) findViewById(R.id.btn_selectImage);
-        ivSelectedImage = (ImageView) findViewById(R.id.iv_selectedImage);
-        fabPublishPost = (FloatingActionButton) findViewById(R.id.fab_publishPost);
+        shareWebView = (WebView) findViewById(R.id.shareWebView);
+        assert shareWebView != null;
+        WebSettings webSettings = shareWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+
+        if(Build.VERSION.SDK_INT >= 21){
+            webSettings.setMixedContentMode(0);
+            shareWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }else if(Build.VERSION.SDK_INT >= 19){
+            shareWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }else {
+            shareWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+        shareWebView.setWebViewClient(new Callback());
+        shareWebView.loadUrl("https://gecdahod.ac.in/campusconnect/a/temp_post_check.php"); //add your test web/page address here
+        shareWebView.setWebChromeClient(new WebChromeClient() {
+            /*
+             * openFileChooser is not a public Android API and has never been part of the SDK.
+             */
+            //handling input[type="file"] requests for android API 16+
+            @SuppressLint("ObsoleteSdkInt")
+            @SuppressWarnings("unused")
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                mUM = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                if (multiple_files && Build.VERSION.SDK_INT >= 18) {
+                    i.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                }
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FCR);
+            }
+
+            //handling input[type="file"] requests for android API 21+
+            @SuppressLint("InlinedApi")
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                if (file_permission()) {
+                    String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA};
+
+                    //checking for storage permission to write images for upload
+                    if (ContextCompat.checkSelfPermission(ShareActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(ShareActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(ShareActivity.this, perms, FCR);
+
+                        //checking for WRITE_EXTERNAL_STORAGE permission
+                    } else if (ContextCompat.checkSelfPermission(ShareActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(ShareActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, FCR);
+
+                        //checking for CAMERA permissions
+                    } else if (ContextCompat.checkSelfPermission(ShareActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(ShareActivity.this, new String[]{Manifest.permission.CAMERA}, FCR);
+                    }
+                    if (mUMA != null) {
+                        mUMA.onReceiveValue(null);
+                    }
+                    mUMA = filePathCallback;
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(ShareActivity.this.getPackageManager()) != null) {
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                            takePictureIntent.putExtra("PhotoPath", mCM);
+                        } catch (IOException ex) {
+                            Log.e("Error : ", "Image file creation failed", ex);
+                        }
+                        if (photoFile != null) {
+                            mCM = "file:" + photoFile.getAbsolutePath();
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                        } else {
+                            takePictureIntent = null;
+                        }
+                    }
+                    Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                    contentSelectionIntent.setType("*/*");
+                    if (multiple_files) {
+                        contentSelectionIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                    }
+                    Intent[] intentArray;
+                    if (takePictureIntent != null) {
+                        intentArray = new Intent[]{takePictureIntent};
+                    } else {
+                        intentArray = new Intent[0];
+                    }
+
+                    Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                    chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                    chooserIntent.putExtra(Intent.EXTRA_TITLE, "File Chooser");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                    startActivityForResult(chooserIntent, FCR);
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        });
+
         setupBottomNavigation();
-        initWidgets();
-
-        btnSelectImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent gallery = new Intent();
-                gallery.setType("image/*");
-                gallery.setAction(Intent.ACTION_GET_CONTENT);
-
-                startActivityForResult(Intent.createChooser(gallery,"Select Image"),PICK_IMAGE);
-            }
-        });
-
-        fabPublishPost.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                verifyPost();
-            }
-        });
     }
 
+    //callback reporting if error occurs
+    public class Callback extends WebViewClient{
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+            Toast.makeText(getApplicationContext(), "Failed loading app!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean file_permission(){
+        if(Build.VERSION.SDK_INT >=23 && (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(ShareActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    //creating new image file here
+    private File createImageFile() throws IOException{
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_"+timeStamp+"_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName,".jpg",storageDir);
+    }
+
+    //back/down key handling
+    @Override
+    public boolean onKeyDown(int keyCode, @NonNull KeyEvent event){
+        if(event.getAction() == KeyEvent.ACTION_DOWN){
+            switch(keyCode){
+                case KeyEvent.KEYCODE_BACK:
+                    if(shareWebView.canGoBack()){
+                        shareWebView.goBack();
+                    }else{
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK){
-
-            imageUri = data.getData();
-            CropImage.activity(imageUri)
-                    .setGuidelines(CropImageView.Guidelines.ON)
-                    .start(this);
-        }
-
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK) {
-                imageUri = result.getUri();
-                ivSelectedImage.setVisibility(View.VISIBLE);
-                ivSelectedImage.setImageURI(imageUri);
-                imageSelect = 1;
-            } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
-                Toast.makeText(mContext, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
     }
 
 
-    private void verifyPost() {
-        if(spinnerUserCategory.getSelectedItem().toString().equals("Select")){
-            Snackbar.make(getCurrentFocus(), "Select a Category.", Snackbar.LENGTH_SHORT).show();
-            return;
-        }
-        if(inputPostCaption.getText().toString().isEmpty() || inputPostCaption.getText().toString().length()<=40){
-            Snackbar.make(getCurrentFocus(), "Write a caption of post (minimum 40 characters).", Snackbar.LENGTH_SHORT).show();
-            inputPostCaption.requestFocus();
-            return;
-        }
-        if(!inputPostLink.getText().toString().isEmpty()) {
-            if (!Patterns.WEB_URL.matcher(inputPostLink.getText().toString()).matches()) {
-                Snackbar.make(getCurrentFocus(), "Enter a valid Link Url.", Snackbar.LENGTH_SHORT).show();
-                return;
-            }
-        }
-
-        publishPost();
-
-    }
-
-    private void publishPost() {
-        Snackbar.make(getCurrentFocus(), "Validation Successful", Snackbar.LENGTH_SHORT).show();
-
-    }
-
-    private void initWidgets() {
-        List<String> dummy = new ArrayList<>();
-        dummy.add("Select");
-        dummy.add("Cat 1");
-        dummy.add("Cat 2");
-        dummy.add("Cat 3");
-        dummy.add("Cat 4");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, dummy);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerUserCategory.setAdapter(adapter);
-
-    }
 
     //Function for the making the bottom navigation Bar in each main Activities.
     private void setupBottomNavigation() {
